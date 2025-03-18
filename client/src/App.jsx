56dc7795fc9db39b1e844808734659979fecc2f4
petkolatev <petkorlatev@gmail.com>
2025-03-18 18:43:59 +0200

    add create book

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import Footer from '../components/footer/Footer'
 import './App.css'
 // import Home from '../components/Home'
 import Catalog from '../components/catalog/Catalog'
-// import CreateBook from '../components/CreateBook'
+import CreateBook from '../components/createBook/CreateBook'
 // import Register from '../components/Register'
 // import Login from '../components/Login'
 // import SingleBook from '../components/SingleBook'
@@ -18,11 +18,11 @@ function App() {
     <>
       <Header />
       <Routes>
+        {/* <Route path='/' element={<Home />} /> */}
         <Route path='/catalog' element={<Catalog />} />
-        {/* <Route path='/' element={<Home />} />
-        <Route path='/catalog/:bookId' element={<SingleBook />} />
         <Route path='/create' element={<CreateBook />} />
-        <Route path='/register' element={<Register />} />
+        {/* <Route path='/catalog/:bookId' element={<SingleBook />} /> */}
+        {/* <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} /> */}
       </Routes>
 
