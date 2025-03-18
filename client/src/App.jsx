709e9fa7ddc8aa634c30f8e5311a6204b1ae572e
petkolatev <petkorlatev@gmail.com>
2025-03-18 18:26:01 +0200

    Create header and footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route } from 'react-router'
+
+import Header from '../components/header/Header'
+import Footer from '../components/footer/Footer'
 import './App.css'
+// import '../components/CreateBook.css'
+// import Home from '../components/Home'
+// import Catalog from '../components/Catalog'
+// import CreateBook from '../components/CreateBook'
+// import Register from '../components/Register'
+// import Login from '../components/Login'
+// import SingleBook from '../components/SingleBook'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <Routes>
+        {/* <Route path='/' element={<Home />} />
+        <Route path='/catalog' element={<Catalog />} />
+        <Route path='/catalog/:bookId' element={<SingleBook />} />
+        <Route path='/create' element={<CreateBook />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} /> */}
+      </Routes>
+
+
+      <Footer />
     </>
   )
 }
