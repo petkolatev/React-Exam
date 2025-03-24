00fbedf53f7cf55f30c008ae4936e7fce29152bc
petkolatev <petkorlatev@gmail.com>
2025-03-24 20:24:41 +0200

    safe user and emal with wrong repass

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -2,16 +2,18 @@
 import { useNavigate } from 'react-router'
 import './register.css'
 import { useRegister } from '../../api/authApi'
+import { useState } from 'react'
 
 const initialValues = { username: '', email: '' }
 
 export default function Register() {
-
+    const [input, setInput] = useState(initialValues)
     const navigate = useNavigate()
     const { register } = useRegister()
+
     const registerHandler = async (userData) => {
         const data = Object.fromEntries(userData)
-
+        setInput(data)
         const { password, rePassword } = data
 
         if (password !== rePassword) {
@@ -29,9 +31,9 @@ export default function Register() {
             <h2>Регистрация</h2>
             <form action={registerHandler}>
                 <label htmlFor="username">Потребителско име</label>
-                <input type="text" id="username" name="username" placeholder="username" defaultValue={initialValues.username} required />
+                <input type="text" id="username" name="username" placeholder="username" defaultValue={input.username} required />
                 <label htmlFor="email">Имейл</label>
-                <input type="email" id="email" name="email" placeholder="email" required />
+                <input type="email" id="email" name="email" placeholder="email" defaultValue={input.email} required />
 
                 <label htmlFor="password">Парола</label>
                 <input type="password" id="password" name="password"
