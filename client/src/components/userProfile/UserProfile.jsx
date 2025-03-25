104c33278fe5214d8e792d9a742af4e4d718a1ca
petkolatev <petkorlatev@gmail.com>
2025-03-25 17:29:42 +0200

    add created and liked book from user

--- client/src/components/userProfile/UserProfile.jsx
+++ client/src/components/userProfile/UserProfile.jsx
@@ -1,13 +1,19 @@
-import { useContext, useEffect } from "react"
+import { useContext } from "react"
 import { UserContex } from "../contexts/UserContex"
 import { useUpdateProfile } from "../../api/authApi"
 import { useNavigate } from "react-router"
+import { useBooks } from "../../api/bookApi"
+import BookItem from "../bookItem/BookItem"
 
 export default function UserProfile() {
+    const { books } = useBooks()
     const navigate = useNavigate()
     const { user, token, userUpdate } = useContext(UserContex)
     const { update } = useUpdateProfile()
     let newUpdatedData = { username: user?.username, email: user?.email, _id: user?._id }
+    const filtredBooks = books.filter((book) => book.owner === user._id || book.likes.includes(user._id))
+
+    console.log(filtredBooks);
 
     const userUpdateProfile = async (userData) => {
         const data = Object.fromEntries(userData)
@@ -22,7 +28,7 @@ export default function UserProfile() {
                 __v: 0
             }
         }
-        
+
         userUpdate(newUpdatedData)
         navigate(`/profile/${user._id}`)
     }
@@ -32,21 +38,29 @@ export default function UserProfile() {
 
 
     return (
-        <div className="row">
-            <div className="column side">
-                <h2>Редактиране на профила</h2>
-                <div className="register-form">
-                    <form action={userUpdateProfile}>
-                        <label htmlFor="username">Потребителско име</label>
-                        <input type="text" id="username" name="username" placeholder="username" defaultValue={username} />
-                        <label htmlFor="email">Имейл</label>
-                        <input type="email" id="email" name="email" placeholder="email" defaultValue={email} />
-                        <button>Потвърди</button>
-                    </form>
+        <>
+            <div className="row">
+                <div className="column side">
+                    <h2>Редактиране на профила</h2>
+                    <div className="register-form">
+                        <form action={userUpdateProfile}>
+                            <label htmlFor="username">Потребителско име</label>
+                            <input type="text" id="username" name="username" placeholder="username" defaultValue={username} />
+                            <label htmlFor="email">Имейл</label>
+                            <input type="email" id="email" name="email" placeholder="email" defaultValue={email} />
+                            <button>Потвърди</button>
+                        </form>
 
+                    </div>
                 </div>
             </div>
-        </div>
+            <div className="catalog section">
+
+                {filtredBooks.map((book) =>
+                    <BookItem key={book._id} book={book} />
+                )}
+            </div>
+        </>
     )
 
 }
